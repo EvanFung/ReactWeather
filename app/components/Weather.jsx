daab5e78a2921601502ffb55ce12fa8c354c4c87
EvanFung <evanfungv@gmail.com>
2016-12-30 05:00:57 +0800

    033 faking our call

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,12 +2,25 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var Weather = React.createClass({
+  getInitialState : function () {
+    return{
+      location: 'Miami',
+      temp:38
+    }
+  },
+  handleSearch: function(location) {
+    this.setState({
+      location : location,
+      temp: 23
+    })
+  },
   render : function () {
+    var {temp,location} = this.state;
     return(
       <div>
         <h3>Weather Component</h3>
-        <WeatherForm />
-        <WeatherMessage />
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage temp={temp} location={location}/>
       </div>
     )
   }
