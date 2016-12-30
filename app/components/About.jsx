1287e1d81d2fcade00f8bbf2d95bec864c8d487a
EvanFung <evanfungv@gmail.com>
2016-12-31 06:55:17 +0800

    Style about page & link to resource

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,20 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h3 className="text-center">About</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+         voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+         sunt in culpa qui officia deserunt mollit
+          anim id est laborum.
+      </p>
+       <p>
+         Here ar some of the tool i used :
+       </p>
+       <ul>
+         <li>
+           <a href="www.facebook.github.io/react">React - Javascript Framework</a>
+         </li>
+       </ul>
     </div>
   )
 };
