75e398b883ad6fbb125e60025a81eaa51a636fef
EvanFung <evanfungv@gmail.com>
2016-12-31 01:11:09 +0800

    040 refactoring stateless function components

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,17 +1,27 @@
 var React = require('react');
 var {Link,IndexLink} = require('react-router');
 //IndexRoute的page要配合用IndexLink
-var Nav = React.createClass({
-  render: function () {
-    return(
-      <div>
-        <h2>Nav component</h2>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get About</Link>
-        <Link to="/example" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Example</Link>
-      </div>
-    );
-  }
-});
+// var Nav = React.createClass({
+//   render: function () {
+//     return(
+//       <div>
+//         <h2>Nav component</h2>
+//         <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
+//         <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get About</Link>
+//         <Link to="/example" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Example</Link>
+//       </div>
+//     );
+//   }
+// });
 
+var Nav = (props) => {
+  return(
+    <div>
+      <h2>Nav component</h2>
+      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather </IndexLink>
+      <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get About</Link>
+      <Link to="/example" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Example</Link>
+    </div>
+  );
+};
 module.exports = Nav;
