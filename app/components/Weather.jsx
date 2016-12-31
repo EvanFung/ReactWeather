282a91a69fcad0d573f41c6d62c85d2ba57c7930
EvanFung <evanfungv@gmail.com>
2016-12-31 08:16:19 +0800

    error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,9 +2,13 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var OpenWeatherMap = require('OpenWeatherMap');
+var ErrorModal = require('ErrorModal');
 var Weather = React.createClass({
     getInitialState: function() {
-        return {isLoading: false}
+        return {
+          isLoading: false,
+          errorMessage: undefined
+        }
     },
     handleSearch: function(location) {
         var that = this;
@@ -12,31 +16,39 @@ var Weather = React.createClass({
 
 
         OpenWeatherMap.getTemp(location).then(function(temp) {
-            that.setState({location: location, temp: temp,isLoading:false})
-        }, function(errorMessage) {
-            that.setState({isLoading:false});
-            alert(errorMessage);
+            that.setState({location: location, temp: temp,isLoading:false,errorMessage:undefined})
+        }, function(e) {
+            that.setState({
+              isLoading:false,
+              errorMessage:e.message
+            });
         });
-        // this.setState({
-        //   location : location,
-        //   temp: 23
-        // })
     },
     render: function() {
-        var {isLoading, temp, location} = this.state;
+        var {isLoading, temp, location,errorMessage} = this.state;
 
         function renderMessage() {
           if (isLoading) {
-            return <h3>Fetching weather...</h3>
+            return <h3 className="text-center">Fetching weather...</h3>
           } else if(temp && location) {
             return <WeatherMessage temp={temp} location={location}/>
           }
         }
+
+        function renderError() {
+          if(typeof errorMessage === 'string') {
+            return (
+              <ErrorModal message={errorMessage}/>
+            )
+          }
+        }
+
         return (
             <div>
-                <h3>Weather Component</h3>
+                <h1 className="text-center">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
+                {renderError()}
             </div>
         )
     }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -13,7 +13,7 @@ var React = require('react');
 var WeatherMessage = (props) => {
   var {temp,location} = props;
   return(
-    <h3>It's it {temp} in {location}</h3>
+    <h2>It's it {temp} in {location}</h2>
   )
 };
 module.exports = WeatherMessage;
